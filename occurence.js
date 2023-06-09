d37f19ae7bc586e2813884b8af211073b5575c91
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2023-06-09 21:57:08 +0530

    Create occurence.js

---
+++ occurence.js
@@ -0,0 +1,8 @@
+let haystack = "sadbutsad", needle = "sad"
+function findOccurrence(str,target){
+    
+    let split=str.indexOf(target)
+    
+    return split;
+}
+console.log(findOccurrence(haystack,needle));
